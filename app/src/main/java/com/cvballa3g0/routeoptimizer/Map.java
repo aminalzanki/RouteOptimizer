051229c225208f56d9d1d3f4b97667c98c27b200
Anthony Alves <aalves2012@my.fit.edu>
2014-08-31 22:04:18 -0400

    Initial commit

---
+++ app/src/main/java/com/cvballa3g0/routeoptimizer/Map.java
@@ -0,0 +1,25 @@
+package com.cvballa3g0.routeoptimizer;
+import com.google.*;
+
+
+public class Map extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_activity);
+
+        // Get a handle to the Map Fragment
+        GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+    }
+}
