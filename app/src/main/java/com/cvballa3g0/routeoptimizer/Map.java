27e66f98ac434f7873517499e2605546054a8260
Anthony Alves <aalves2012@my.fit.edu>
2014-09-06 02:08:44 -0400

    started working on the google places autocomplete feature

--- app/src/main/java/com/cvballa3g0/routeoptimizer/Map.java
+++ app/src/main/java/com/cvballa3g0/routeoptimizer/Map.java
@@ -13,73 +13,71 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
 
-public class Map  extends Fragment {
+public class Map extends Fragment {
 
     private static View view;
+
 
     private static GoogleMap mMap;
     private static Double latitude, longitude;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        if (container == null) {
-            return null;
-        }
         view = (RelativeLayout) inflater.inflate(R.layout.map_layout, container, false);
-        // Passing harcoded values for latitude & longitude. Please change as per your need. This is just used to drop a Marker on the Map
-        latitude = 26.78;
-        longitude = 72.56;
-
         setUpMapIfNeeded(); // For setting up the MapFragment
         return view;
     }
 
-    /***** Sets up the map if it is possible to do so *****/
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    public void onPause() {
+        super.onPause();
+    }
+
+    public void onResume() {
+        super.onResume();
+    }
+
+    // Sets up the map
     public static void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((MapFragment) MainDrawer.fragmentManager
-                    .findFragmentById(R.id.map)).getMap();
-            // Check if we were successful in obtaining the map.
+            mMap = ((MapFragment) MainDrawer.fragmentManager.findFragmentById(R.id.map)).getMap();
+            // Double checking the mMap variable
             if (mMap != null)
-                setUpMap();
+                initializeMap();
         }
     }
-    private static void setUpMap() {
-        // For showing a move to my loction button
+    private static void initializeMap() {
+        // Move to current Location button
         mMap.setMyLocationEnabled(true);
-        // For dropping a marker at a point on the Map
-        // For zooming automatically to the Dropped PIN Location
-        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(39.8282, -98.5795), 6.0f));
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(39.8282, -98.5795), 3.0f));
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         if (mMap != null)
-            setUpMap();
+            initializeMap();
 
         if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
             mMap = ((MapFragment) MainDrawer.fragmentManager
                     .findFragmentById(R.id.map)).getMap();
-            // Check if we were successful in obtaining the map.
+
             if (mMap != null)
-                setUpMap();
+                initializeMap();
         }
     }
 
-    /**** The mapfragment's id must be removed from the FragmentManager
-     **** or else if the same it is passed on the next time then
-     **** app will crash ****/
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         if (mMap != null) {
             MainDrawer.fragmentManager.beginTransaction()
-                    .remove(MainDrawer.fragmentManager.findFragmentById(R.id.map)).commit();
+                      .remove(MainDrawer.fragmentManager
+                      .findFragmentById(R.id.map)).commit();
             mMap = null;
         }
     }
